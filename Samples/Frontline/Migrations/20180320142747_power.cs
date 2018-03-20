850636fca93c28b912c841c9e60da792fb3ec71f
Rong <crazor@foxmail.com>
2018-03-20 22:35:23 +0800

    db

---
+++ Samples/Frontline/Migrations/20180320142747_power.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Frontline.Migrations
+{
+    public partial class power : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Power",
+                table: "Units",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Power",
+                table: "Units");
+        }
+    }
+}
